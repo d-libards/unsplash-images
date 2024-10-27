e0ec9ad578824b9f91c226949f60c46cd76bb361
d-libards <dominiquelibardo09@gmail.com>
2024-10-28 05:08:36 +0800

    dark mode

--- src/context.jsx
+++ src/context.jsx
@@ -2,16 +2,32 @@ import { useContext, createContext, useState, useEffect } from 'react';
 
 const AppContext = createContext();
 
+function getInitialDarkMode() {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+  const storedDarkMode = localStorage.getItem('darkTheme');
+
+  if (storedDarkMode === null) {
+    return prefersDarkMode;
+  }
+
+  return storedDarkMode === 'true';
+}
+
 export function AppProvider({ children }) {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState('dogs');
 
   function toggleDarkTheme() {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
   }
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, [isDarkTheme]);
 
   return (
     <AppContext.Provider
